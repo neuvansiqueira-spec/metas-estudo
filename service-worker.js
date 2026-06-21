1b3882e616ab184f2c26c418f98129bdc28da22d
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-06-21 19:07:47 -0300

    Renomeia histórico geral

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "metas-estudo-cache-20260621-question-history-layout-fix";
+const CACHE_NAME = "metas-estudo-cache-20260621-history-name-fix";
 const FILES_TO_CACHE = [
   "./",
   "index.html",
