8190b56637101ca9d46568c91d0a0a4d895e0690
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-07-10 16:46:21 -0300

    Fix GitHub Pages deployment source

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "metas-estudo-20260710-factory-operational-fix1";
+const CACHE_NAME = "metas-estudo-20260710-pages-deploy-fix1";
 const FILES_TO_CACHE = [
   "./",
   "index.html",
