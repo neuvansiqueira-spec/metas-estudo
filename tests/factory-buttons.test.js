e7fe6efd5aa539bad35f75409178b781677aa722
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-07-13 20:51:08 -0300

    Adiciona análise estratégica do desempenho

--- tests/factory-buttons.test.js
+++ tests/factory-buttons.test.js
@@ -10,7 +10,7 @@ const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const serviceWorker = fs.readFileSync('service-worker.js', 'utf8');
 const docsServiceWorker = fs.readFileSync('docs/service-worker.js', 'utf8');
 
-const version = '20260713-corrige-plano-dia-mobile';
+const version = '20260713-analise-estrategica-v1';
 const clickRoutes = [
   'factoryPrompt', 'factoryPromptClose', 'factoryPromptCopy', 'factoryRouterCopy',
   'factoryEdit', 'factoryDelete', 'factoryModules', 'factoryModulesCancel',
