9689afa2c94b9d0ec878109c15f1e9cb216604fa
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-07-13 12:51:50 -0300

    Reverte atualização duplicada da aba Materiais

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "metas-estudo-20260712-materiais-detalhes-recolhiveis";
+const CACHE_NAME = "metas-estudo-20260713-corrige-duplicidade-materiais";
 const FILES_TO_CACHE = [
   "./",
   "index.html",
