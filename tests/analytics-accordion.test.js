e5a73914749ac695e0b8235a95003eab03f684bd
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-07-14 15:07:11 -0300

    Corrige exportação real dos gráficos

--- tests/analytics-accordion.test.js
+++ tests/analytics-accordion.test.js
@@ -7,7 +7,7 @@ const script = fs.readFileSync('script.js','utf8');
 const html = fs.readFileSync('index.html','utf8');
 const style = fs.readFileSync('style.css','utf8');
 const sw = fs.readFileSync('service-worker.js','utf8');
-const version = '20260714-exportacoes-didaticas-mais-opcoes-v1';
+const version = '20260714-exportacao-real-graficos-v2';
 
 function logic(){
   const start = script.indexOf('function formatExportDuration(minutes');
