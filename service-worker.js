a2eabd5eeaaef98e4a15a6554c778884ffb7b8b2
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-07-14 08:29:25 -0300

    Moderniza fluxo de registro do cronometro

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // versão anterior: metas-estudo-20260714-conselheiro-local-v1
-const CACHE_NAME = "metas-estudo-20260714-safe-area-mensagens-alarme-cronometro-v3";
+const CACHE_NAME = "metas-estudo-20260714-cronometro-fluxo-moderno-v1";
 const FILES_TO_CACHE = [
   "./",
   "index.html",
