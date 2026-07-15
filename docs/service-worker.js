6d94b2e97d354dc2ce07fdc0220f73c8c68d6197
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-07-14 21:01:18 -0300

    Corrige conclusão de meta sem repetir tempo

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
 // versão anterior: metas-estudo-20260714-conselheiro-local-v1
-const CACHE_NAME = "metas-estudo-20260714-hotfix-planejamento-mobile-final-v1";
+const CACHE_NAME = "metas-estudo-20260714-conclusao-meta-sem-repetir-tempo-v1";
 const FILES_TO_CACHE = [
   "./",
   "index.html",
