d5b8cf13c2fbeeb2199f553157a6d4e3662b9ce7
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-07-16 16:24:59 -0300

    Deixa Revisão Inteligente recolhível no Plano do Dia
    A seção inicia fechada e pode ser aberta ou recolhida sem alterar os dados das revisões.

--- tests/factory-buttons.test.js
+++ tests/factory-buttons.test.js
@@ -10,7 +10,7 @@ const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const serviceWorker = fs.readFileSync('service-worker.js', 'utf8');
 const docsServiceWorker = fs.readFileSync('docs/service-worker.js', 'utf8');
 
-const version = '20260716-consistencia-materiais-v2';
+const version = '20260716-revisao-inteligente-recolhivel-v1';
 const clickRoutes = [
   'factoryPrompt', 'factoryPromptClose', 'factoryPromptCopy', 'factoryRouterCopy',
   'factoryEdit', 'factoryDelete', 'factoryModules', 'factoryModulesCancel',
