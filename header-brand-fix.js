9cd5f6dd1c40773724ec3fdae26c5efcb977a048
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-07-20 06:05:09 -0300

    Integra fabrica e materiais ao planejamento

--- header-brand-fix.js
+++ header-brand-fix.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const THEME_VERSION = "20260720-calendario-mensal-v79";
+  const THEME_VERSION = "20260720-integracao-fabrica-materiais-v80";
   const DESIRED_HTML = `
     <div class="brand-copy">
       <strong>Aldus Metas Concurso</strong>
--- service-worker.js
+++ service-worker.js
@@ -46,9 +46,10 @@ const PREVIOUS_DEPLOYMENT_VERSIONS = [
   "20260719-correcao-metas-v75",
   "20260720-concluidas-visibilidade-v76",
   "20260720-distribuicao-reposicao-v77",
-  "20260720-calendario-semanal-v78"
+  "20260720-calendario-semanal-v78",
+  "20260720-calendario-mensal-v79"
 ];
-const CURRENT_VERSION = "20260720-calendario-mensal-v79";
+const CURRENT_VERSION = "20260720-integracao-fabrica-materiais-v80";
 const CACHE_NAME = `metas-estudo-${CURRENT_VERSION}`;
 // Cache anterior reconhecido para limpeza: startup-v25.
 const ASSET_CACHE_NAME = `${CACHE_NAME}-startup-v26`;
