64d98b7f6d715bdff85e75bc12d0d3bb0e63d37a
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-07-29 06:34:28 -0300

    Integra metas, materiais e fábrica pelo edital (#332)
    Centraliza descrições no item exato do Edital Verticalizado e integra metas, plano diário, calendário, planejamento, materiais, fábrica e cronômetro por UUID.

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CURRENT_VERSION = "20260728-cronometro-imediato-v169";
+const CURRENT_VERSION = "20260729-vinculos-integrados-v169";
 const CACHE_NAME = `metas-estudo-${CURRENT_VERSION}`;
 const STATIC_ASSETS = [
   "./",
