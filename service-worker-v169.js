f24b176a25004c59107e8a7207a92ba1fd794140
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-08-02 06:06:46 -0300

    Ativa imediatamente o runtime de atualização V217

--- service-worker-v169.js
+++ service-worker-v169.js
@@ -1,2 +1,4 @@
-/* Aldus service worker entry: 20260802-tons-azulados-historico-v216 */
-importScripts("./runtime-entry-v216.js");
+/* Aldus service worker entry: 20260802-corrige-atualizacao-versao-v217 */
+self.addEventListener("install", (event) => event.waitUntil(self.skipWaiting()));
+self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+importScripts("./runtime-entry-v217.js");
