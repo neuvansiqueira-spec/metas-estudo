480fe0a5f645d46b61c3186d189efe515a63fe15
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-08-25 16:38:03 -0300

    V396: validar bootstrap, precache e performance

--- tests/v396-timer-audio-stability.test.js
+++ tests/v396-timer-audio-stability.test.js
@@ -127,8 +127,36 @@ test("observador deixa de vigiar o body inteiro após encontrar os dois alvos", 
   assert.match(source, /targetObserver\.observe\(element/);
 });
 
-test("raiz e docs publicam exatamente a mesma V396", () => {
-  assert.equal(source, fs.readFileSync(DOCS, "utf8"));
+test("V396 entra antes do núcleo nos dois bootstraps", () => {
+  const protectedLoader = fs.readFileSync("bootstrap-integrity-loader-v275.js", "utf8");
+  const legacyLoader = fs.readFileSync("bootstrap-integrity-loader-v258.js", "utf8");
+  for (const loader of [protectedLoader, legacyLoader]) {
+    assert.match(loader, /TIMER_AUDIO_STABILITY_SCRIPT = "timer-audio-stability-v396\.js\?v=20260825-timer-audio-stability-v396"/);
+    assert.ok(loader.indexOf("parent.insertBefore(timerAudioStability") < loader.indexOf("parent.insertBefore(core"));
+  }
+  assert.match(protectedLoader, /await timerAudioReady/);
+});
+
+test("worker renova o cache e precacheia V396 sem remover fast paths", () => {
+  const worker = fs.readFileSync("service-worker.js", "utf8");
+  assert.match(worker, /TIMER_AUDIO_STABILITY_VERSION = "20260825-timer-audio-stability-v396"/);
+  assert.match(worker, /timer-audio-stability-v396/);
+  assert.match(worker, /TIMER_AUDIO_STABILITY,/);
+  assert.match(worker, /&update=v395&audio=v396/);
+  assert.match(worker, /async function cachedFirstNavigation/);
+  assert.match(worker, /async function cacheFirstStatic/);
+});
+
+test("raiz e docs publicam exatamente a mesma V396 e mesmos bootstraps/workers", () => {
+  for (const file of [
+    ROOT,
+    "bootstrap-integrity-loader-v275.js",
+    "bootstrap-integrity-loader-v258.js",
+    "service-worker.js",
+    "service-worker-v378.js"
+  ]) {
+    assert.equal(fs.readFileSync(file, "utf8"), fs.readFileSync(`docs/${file}`, "utf8"), `${file} deve permanecer idêntico em docs`);
+  }
   assert.match(source, /20260825-timer-audio-stability-v396/);
   assert.match(source, /focus-minutes:/);
 });
