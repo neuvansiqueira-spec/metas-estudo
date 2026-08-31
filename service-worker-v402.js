6e82bee57958ba6ec36cfaa05aa4badcb71b2572
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-08-31 00:29:53 -0300

    V414: corrigir loop do aviso de atualização

--- service-worker-v402.js
+++ service-worker-v402.js
@@ -1,10 +1,11 @@
 "use strict";
 
-const CACHE_FIX_VERSION = "20260831-core-daily-plan-consistency-v413";
+const CACHE_FIX_VERSION = "20260831-update-banner-stability-v414";
 const COMPLETED_GOALS_CACHE_VERSION_V411 = "20260831-metas-concluidas-somente-revisao-v411";
 const DAILY_GOAL_STABILITY_CACHE_VERSION_V412 = "20260831-estabilidade-metas-antes-bootstrap-v412";
 const CORE_DAILY_PLAN_CACHE_VERSION_V413 = "20260831-core-daily-plan-consistency-v413";
 const CURRENT_HTML_GUARD_VERSION_V413 = "20260831-rejeita-html-v402-em-cache-v413";
+const UPDATE_BANNER_STABILITY_VERSION_V414 = "20260831-update-banner-stability-v414";
 // Contrato exclusivamente textual do pipeline V395. O bloco mantém CACHE_NAME
 // fora do escopo léxico global para não colidir com o CACHE_NAME declarado pelo
 // service-worker.js canônico carregado via importScripts.
@@ -16,8 +17,11 @@ const PLANNING_INTEGRITY_PATHS = new Set([
   new URL("index.html", self.registration.scope).pathname,
   new URL("bootstrap-integrity-loader-v258.js", self.registration.scope).pathname,
   new URL("bootstrap-integrity-loader-v258-core.js", self.registration.scope).pathname,
+  new URL("bootstrap-integrity-loader-v275.js", self.registration.scope).pathname,
+  new URL("update-flow-v395.js", self.registration.scope).pathname,
   new URL("app-v402.js", self.registration.scope).pathname,
   new URL("app-v413.js", self.registration.scope).pathname,
+  new URL("app-v414.js", self.registration.scope).pathname,
   new URL("startup-planning-stability-v387.js", self.registration.scope).pathname,
   new URL("planning-integrity-v235.js", self.registration.scope).pathname,
   new URL("planning-integrity-loader-v235.js", self.registration.scope).pathname
@@ -49,4 +53,5 @@ void COMPLETED_GOALS_CACHE_VERSION_V411;
 void DAILY_GOAL_STABILITY_CACHE_VERSION_V412;
 void CORE_DAILY_PLAN_CACHE_VERSION_V413;
 void CURRENT_HTML_GUARD_VERSION_V413;
+void UPDATE_BANNER_STABILITY_VERSION_V414;
 importScripts(`service-worker.js?v=${CACHE_FIX_VERSION}`);
