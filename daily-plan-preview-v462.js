39cb8c0ec2dae026225d27d92fac6907a168e9fe
neuvansiqueira-spec <neuvan.siqueira@gmail.com>
2026-09-07 10:03:55 -0300

    Iguala altura e peso do título da V429 à prévia V456

--- daily-plan-preview-v462.js
+++ daily-plan-preview-v462.js
@@ -1,7 +1,7 @@
 /* Apresentacao do Plano do Dia conforme a previa V456. Nao escreve dados. */
 (() => {
   "use strict";
-  const VERSION = "20260907-plano-dia-previa-fiel-v462";
+  const VERSION = "20260907-plano-dia-previa-fiel-v463";
   const KEY = "__ALDUS_DAILY_PLAN_PREVIEW_V462__";
   const ROOT = "view-metas-do-dia";
   const FRAME = "aldusDailyPlanFrameV462";
